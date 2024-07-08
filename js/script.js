379500f37cce68bb3caddd9b6d33db07f48a96ca
Arnav Chahal <chahal@Arnavs-MacBook-Pro.local>
2024-07-08 00:50:14 -0500

    done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,13 @@ if (document.getElementById('my-work-link')) {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
   })
 }
+document.getElementById('dark-mode-toggle').addEventListener('change', function() {
+  document.body.classList.toggle('dark-mode');
+  document.querySelectorAll('.navbar, .button, .footer-icon, .icon-link, .subheader-text').forEach(function(element) {
+      element.classList.toggle('dark-mode');
+  });
+});
+
+function scrollToSection(sectionId) {
+  document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
